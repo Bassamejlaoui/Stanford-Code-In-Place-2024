5fd840e1545a947b8700becfe8cc9bd1f25e6a90
Bassam Mejlaoui (The CyberPolyglot) <130122304+mejbass@users.noreply.github.com>
2024-06-14 19:13:01 +0200

    Create Stone Mason Karel.py

---
+++ Stone Mason Karel.py
@@ -0,0 +1,85 @@
+from karel.stanfordkarel import *
+
+"""
+File: main.py
+--------------------
+When you finish writing this file, Karel should have repaired 
+each of the columns in the temple
+"""
+
+def main():
+    """
+    You should write your code to make Karel do its task in
+    this function. Make sure to delete the 'pass' line before
+    starting to write your own code. You should also delete this
+    comment and replace it with a better, more descriptive one.
+    """
+    pass
+    turn_left()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    turn_right()
+    move()
+    move()
+    move()
+    move()
+    turn_right()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    turn_right()
+    move()
+    move()
+    move()
+    move()
+    put_beeper()
+    turn_right()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    turn_right()
+    turn_right()
+    turn_right()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+if __name__ == '__main__':
+    main()
